f37b8ab91909497de169a257ee3140e131fd9b38
Alumno <Alumno@Usuario-PC>
2015-09-30 09:44:13 -0500

    primer commit python

---
+++ src/mostrar.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Alumno"
+__date__ = "$30/09/2015 09:34:28 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
